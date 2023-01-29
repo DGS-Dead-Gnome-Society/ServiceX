fb7a064a10e69ecc76ea9e24d355f2d9941793b6
Helena Bolan <elinboo16@gmail.com>
2023-01-29 23:56:53 +0000

    Added 'count' and 'fancy' as options which can be passed to 'channel list'

--- modules/command_channel.py
+++ modules/command_channel.py
@@ -5,29 +5,38 @@ def run(self, target, nickname, message):
         self.msgSend(target, nickname, "A subcommand has not been specified.")
         return
 
-    try:
-        channelName = message[1]
-    except IndexError:
-        channelName = target
+    if subCommand == "join" or "part" or "cycle":
+        try:
+            channelName = message[1]
+        except IndexError:
+            channelName = target
 
-    if subCommand == "join":
-        self.chanJoin(channelName, target)
+        if subCommand == "join":
+            self.chanJoin(channelName, target)
 
-    elif subCommand == "part":
-        self.chanPart(channelName, target)
+        if subCommand == "part":
+            self.chanPart(channelName, target)
 
-    elif subCommand == "cycle":
-        self.chanPart(channelName, target, False)
-        self.chanJoin(channelName, target, False)
+        if subCommand == "cycle":
+            self.chanPart(channelName, target, False)
+            self.chanJoin(channelName, target, False)
 
-    elif subCommand == "list":
-        if len(self.channels) == 0:
-            self.msgSend(target, nickname, "I am not in any channels on this IRC network.")
+    if subCommand == "list":
+        if len(message) == 1:
+            self.msgSend(target, nickname, ", ".join(self.channels))
 
-        elif len(self.channels) == 1:
-            self.msgSend(target, nickname, "I am in %s on this IRC network." % self.channels[0])
+        if message[1] == "count":
+            self.msgSend(target, nickname, len(self.channels))
 
-        else:
-            self.msgSend(target, nickname, "I am in %s and %s on this IRC network." % (", ".join(self.channels[:-1]), self.channels[-1:][0]))
+        if message[1] == "fancy":
+            if len(self.channels) == 0:
+                self.msgSend(target, nickname, "I am not in any channels on this IRC network.")
+
+            elif len(self.channels) == 1:
+                self.msgSend(target, nickname, "I am just in %s on this IRC network." % self.channels[0])
+
+            else:
+                self.msgSend(target, nickname, "I am in %s and %s on this IRC network, a total of %s channels." % (", ".join(self.channels[:-1]), self.channels[-1:][0], len(self.channels)))
+
     else:
         self.msgSend(target, nickname, "Subcommand not found.")
