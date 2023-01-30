9245a7895bceca30e842a88ce7c03ab9e01b78e1
Helena Bolan <elinboo16@gmail.com>
2023-01-30 16:26:51 +0000

    Added modload, modunload and modlist commands

--- modules/utilities.py
+++ modules/utilities.py
@@ -18,3 +18,34 @@ def command_date(self, arguments):
 def command_time(self, arguments):
     target, nickname, message = arguments
     self.msgSend(target, nickname, self.timestamp("time"))
+
+def command_modlist(self, arguments):
+    target, nickname, message = arguments
+    modlist = []
+
+    for module in self.modules:
+        modlist.append(module.__name__)
+
+    self.msgSend(target, nickname, ", ".join(modlist))
+
+def command_modload(self, arguments):
+    target, nickname, message = arguments
+
+    if len(message) == 1:
+        self.loadModule(message[0])
+    elif len(message) > 1:
+        for moduleName in message:
+            self.loadModule(moduleName)
+    else:
+        self.msgSend(target, nickname, "You have not specified a module to load.")
+
+def command_modunload(self, arguments):
+    target, nickname, message = arguments
+
+    if len(message) == 1:
+        self.unloadModule(message[0])
+    elif len(message) > 1:
+        for moduleName in message:
+            self.unloadModule(moduleName)
+    else:
+        self.msgSend(target, nickname, "You have not specified a module to unload.")
