18d6957edecf048197b680e4ca9840209170e19d
Helena Bolan <elinboo16@gmail.com>
2023-01-31 13:49:08 +0000

    Added variable_date() and variable_time() definitions

--- modules/utilities.py
+++ modules/utilities.py
@@ -1,15 +1,8 @@
-def command_echo(self, arguments):
-    target, nickname, message = arguments
+def variable_date(self):
+    return self.timestamp("date")
 
-    if len(message) == 1:
-        self.msgSend(target, nickname, self.variableParse(message[0]))
-
-    elif len(message) > 1:
-        message = " ".join(message)
-        self.msgSend(target, nickname, self.variableParse(message))
-
-    else:
-        self.msgSend(target, nickname, "Not enough parameters.")
+def variable_time(self):
+    return self.timestamp("time")
 
 def command_date(self, arguments):
     target, nickname, message = arguments
@@ -18,6 +11,19 @@ def command_date(self, arguments):
 def command_time(self, arguments):
     target, nickname, message = arguments
     self.msgSend(target, nickname, self.timestamp("time"))
+
+def command_echo(self, arguments):
+    target, nickname, message = arguments
+
+    if len(message) == 1:
+        self.msgSend(target, nickname, self.variableParse(target, nickname, message[0]))
+
+    elif len(message) > 1:
+        message = " ".join(message)
+        self.msgSend(target, nickname, self.variableParse(target, nickname, message))
+
+    else:
+        self.msgSend(target, nickname, "Not enough parameters.")
 
 def command_module(self, arguments):
     target, nickname, message = arguments
